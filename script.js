626f375280b25017f3b0c5cbd1b48bc718e07348
Pedro Neres <124516595+bypedroneres@users.noreply.github.com>
2023-07-11 12:55:11 -0300

    Update script.js
    add a new dot menu for my books section

--- script.js
+++ script.js
@@ -26,3 +26,32 @@ dotMenu.addEventListener('click', (event) => {
     });
   }
 });
+
+// Get the dot menu and gallery images
+const dotMenu2 = document.querySelector('.dot-menu2');
+const BookCovers = document.querySelectorAll('.book-covers');
+
+// Add click event listener to each dot
+dotMenu2.addEventListener('click', (event) => {
+  if (event.target.classList.contains('dot2')) {
+    // Remove active class from all dots
+    dotMenu2.querySelectorAll('.dot2').forEach((dot) => {
+      dot.classList.remove('active');
+    });
+
+    // Add active class to the clicked dot
+    event.target.classList.add('active');
+
+    // Get the index of the clicked dot
+    const dotIndex = Array.from(dotMenu2.children).indexOf(event.target);
+
+    // Display the corresponding gallery image
+    BookCovers.forEach((image, index) => {
+      if (index === dotIndex) {
+        image.style.display = 'block';
+      } else {
+        image.style.display = 'none';
+      }
+    });
+  }
+});
